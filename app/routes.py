cc44264a1cf59fed916a3c3ae8827df37fc4fc3a
Pedro Paredes <gangsterveggies@gmail.com>
2020-04-09 23:21:07 -0400

    Add base template and forms

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,25 @@
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-  return "Hello, World!"
+  user = {'username': 'Tim'}
+  posts = [{
+    'author': {'username': 'Pedro'},
+    'body': 'Test'
+  }, {
+    'author': {'username': 'Tim'},
+    'body': 'Test2'
+  }]
+  return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+  form = LoginForm()
+  if form.validate_on_submit():
+    flash('Login requested for user {}'.format(
+      form.username.data))
+    return redirect(url_for('index'))
+  return render_template('login.html', title='Sign In', form=form)
