35505be244cc64d5e5ea382ac5e3dfe8e7ab2f17
Pedro Paredes <gangsterveggies@gmail.com>
2020-04-11 01:42:29 -0400

    Add account types

--- app/models.py
+++ app/models.py
@@ -1,19 +1,29 @@
 from werkzeug.security import generate_password_hash, check_password_hash
-from app import db
+from app import db, login
 from flask_login import UserMixin
-from app import login
+import enum
+
+class AccountType(enum.Enum):
+  admin = 1
+  donor = 2
+  hospital = 3
+  volunteer = 4
 
 class User(UserMixin, db.Model):
   id = db.Column(db.Integer, primary_key=True)
   username = db.Column(db.String(64), index=True, unique=True)
   email = db.Column(db.String(120), index=True, unique=True)
   password_hash = db.Column(db.String(128))
+  account_type = db.Column(db.Enum(AccountType))
 
   def set_password(self, password):
     self.password_hash = generate_password_hash(password)
 
   def check_password(self, password):
     return check_password_hash(self.password_hash, password)
+
+  def set_account_type(self, account_type):
+    self.account_type = account_type
 
   def __repr__(self):
     return '<User {}>'.format(self.username)    
