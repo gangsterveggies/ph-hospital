3493b89ed9e304c6b478fc68c8f54a92fb03302f
Pedro Paredes <gangsterveggies@gmail.com>
2020-04-16 02:03:30 -0400

    Populate with hospitals

--- app/cli.py
+++ app/cli.py
@@ -1,6 +1,6 @@
 import click, os
 from app import app, db
-from app.models import User, AccountType
+from app.models import User, AccountType, Hospital
 
 @app.cli.command('setup-debug-database')
 def setup_debug_database():
@@ -10,6 +10,13 @@ def setup_debug_database():
     u = User(username='tim', email='tim@test.com', account_type=AccountType.admin)
     u.set_password('tim')
     db.session.add(u)
+    db.session.commit()
+
+    click.echo('Deleting all hospitals and adding default ones')
+    Hospital.query.delete()
+    for i in range(10):
+      h = Hospital(name="UPMC" + str(i + 1), location="41.407845, -75.655145", address="200 Lothrop Street Pittsburgh, PA 15213 ", contact="4120000000")
+      db.session.add(h)
     db.session.commit()
   else:
     click.echo('This command only works in debug mode...')
