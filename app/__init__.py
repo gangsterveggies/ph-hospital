eae68a158ea6e471f196a9b45c59dc54b08a4770
Pedro Paredes <gangsterveggies@gmail.com>
2020-05-07 00:58:26 -0400

    Add timestamp format

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_moment import Moment
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -15,6 +16,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 login.login_message_category = "warning"
 mail = Mail(app)
+moment = Moment(app)
 
 from app import routes, models, errors, helpers
 
