baa2d1cc808612153ca3273e163076669a381c72
Pedro Paredes <gangsterveggies@gmail.com>
2020-05-08 00:12:16 -0400

    Add user info popover

--- app/routes.py
+++ app/routes.py
@@ -289,6 +289,12 @@ def send_donation(id):
 ## Misc pages
 #######################################
 
+@app.route('/user/<username>/popup')
+@login_required
+def user_popup(username):
+  user = User.query.filter_by(username=username).first_or_404()
+  return render_template('user_popup.html', user=user)
+
 @app.route('/profile', methods=['GET', 'POST'])
 @login_required
 def profile():
