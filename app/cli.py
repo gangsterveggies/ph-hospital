ce5eba021de72fa786e2b1530c2c83fcaf00718e
Pedro Paredes <gangsterveggies@gmail.com>
2020-05-18 10:11:33 -0400

    Production change

--- app/cli.py
+++ app/cli.py
@@ -18,62 +18,62 @@ supply_list_pairs = [
 
 @app.cli.command('setup-debug-database')
 def setup_debug_database():
-  if app.debug:
-    click.echo('Deleting all users and adding default users')
+#  if app.debug:
+  click.echo('Deleting all users and adding default users')
 
-    user_list = User.query.all()
-    for u in user_list:
-      u.verified = []
-    db.session.commit()
+  user_list = User.query.all()
+  for u in user_list:
+    u.verified = []
+  db.session.commit()
     
-    User.query.delete()
-    user_tim = User(username='tim', email='tim@test.com', account_type=AccountType.admin)
-    user_tim.verified_tag = True
-    user_tim.set_password('tim')
-    db.session.add(user_tim)
+  User.query.delete()
+  user_tim = User(username='tim', email='tim@test.com', account_type=AccountType.admin)
+  user_tim.verified_tag = True
+  user_tim.set_password('tim')
+  db.session.add(user_tim)
 
-    for i in range(5):
-      u = User(username='donor' + str(i + 1), email='donor' + str(i + 1) + '@test.com', account_type=AccountType.donor, verified_tag=True)
-      u.set_password('donor')
-      db.session.add(u)
+  for i in range(5):
+    u = User(username='donor' + str(i + 1), email='donor' + str(i + 1) + '@test.com', account_type=AccountType.donor, verified_tag=True)
+    u.set_password('donor')
+    db.session.add(u)
     
-    db.session.commit()
+  db.session.commit()
 
-    click.echo('Deleting all hospitals and adding default ones')
-    Hospital.query.delete()
+  click.echo('Deleting all hospitals and adding default ones')
+  Hospital.query.delete()
 
-    fake = Faker()
-    f = lambda x : str(x[0]) + " " + str(x[1])
-    hospital_list = []
-    for i in range(10):
-      h = Hospital(name=(fake.city() + " Hospital"), location=f(fake.local_latlng()), address=fake.address(), contact=fake.phone_number())
-      hospital_list.append(h)
-      db.session.add(h)
-    db.session.commit()
+  fake = Faker()
+  f = lambda x : str(x[0]) + " " + str(x[1])
+  hospital_list = []
+  for i in range(10):
+    h = Hospital(name=(fake.city() + " Hospital"), location=f(fake.local_latlng()), address=fake.address(), contact=fake.phone_number())
+    hospital_list.append(h)
+    db.session.add(h)
+  db.session.commit()
 
-    click.echo('Deleting all donations and request')
-    RequestStatus.query.delete()
-    SingleRequest.query.delete()
-    RequestGroup.query.delete()
-    db.session.commit()
+  click.echo('Deleting all donations and request')
+  RequestStatus.query.delete()
+  SingleRequest.query.delete()
+  RequestGroup.query.delete()
+  db.session.commit()
     
-    for i in range(5):
-      u = User(username='doctor' + str(i + 1), email='doctor' + str(i + 1) + '@test.com', account_type=AccountType.doctor, hospital=random.choice(hospital_list))
-      u.set_password('doctor')
-      db.session.add(u)
+  for i in range(5):
+    u = User(username='doctor' + str(i + 1), email='doctor' + str(i + 1) + '@test.com', account_type=AccountType.doctor, hospital=random.choice(hospital_list))
+    u.set_password('doctor')
+    db.session.add(u)
 
-      for _a in range(3):
-        request = RequestGroup(requester=u)
-        db.session.add(request)
-        for _ in range(4):
-          single_request = SingleRequest(supply_id=random.choice(range(1,len(supply_list_pairs))), request=request, quantity=10, custom_info=fake.paragraph(), show_donors=False)
-          db.session.add(single_request)
-          request_status = RequestStatus(status_type=RequestStatusType.requested, single_request=single_request)
-          db.session.add(request_status)
+    for _a in range(3):
+      request = RequestGroup(requester=u)
+      db.session.add(request)
+      for _ in range(4):
+        single_request = SingleRequest(supply_id=random.choice(range(1,len(supply_list_pairs))), request=request, quantity=10, custom_info=fake.paragraph(), show_donors=False)
+        db.session.add(single_request)
+        request_status = RequestStatus(status_type=RequestStatusType.requested, single_request=single_request)
+        db.session.add(request_status)
 
-    db.session.commit()
-  else:
-    click.echo('This command only works in debug mode...')
+  db.session.commit()
+#  else:
+#    click.echo('This command only works in debug mode...')
 
 @app.cli.command('add-base-ppes')
 def add_base_ppes():
